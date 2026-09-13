246d5977ec0a90ed3e686c8fab9ab8f7cf60bac1
VinayKumarS-11 <vinayskumar2004@gmail.com>
2026-09-13 22:46:38 +0530

    Finding max product of subarray

---
+++ Array/Hard/Maxproduct.py
@@ -0,0 +1,22 @@
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        res = nums[0]
+        maxProd = nums[0]
+        minProd = nums[0]
+
+        for i in range(1, len(nums)):
+            curr = nums[i]
+
+            if curr < 0:
+                maxProd, minProd = minProd, maxProd
+
+            maxProd = max(curr, maxProd * curr)
+            minProd = min(curr, minProd * curr)
+
+            res = max(res, maxProd)
+
+        return res 
+            
+nums = [2, 3, -2, 4]
+sol = Solution()
+print(sol.maxProduct(nums))    
